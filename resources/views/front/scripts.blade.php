4d3ab249c7e5467ac25677e161253d909184998d
Kaluchii <mr.kaluchii@mail.ru>
2018-01-04 00:25:26 +0600

    Test

--- resources/views/front/scripts.blade.php
+++ resources/views/front/scripts.blade.php
@@ -3,7 +3,7 @@
     <script defer src="/js/jquery.magnific-popup.min.js"></script>
     <script defer src="/js/jquery.mask.js"></script>
     <script defer src="/js/slick.min.js"></script>
-    <script defer src="/js/form.js?1"></script>
+    <script defer src="/js/form.js?4"></script>
     <script defer src="https://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>
-    <script defer src="/js/main.js?3"></script>
+    <script defer src="/js/main.js?4"></script>
 @endsection
